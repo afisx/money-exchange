08fdbe27cc8a3d165ed43b12ebb31acde6f89e48
afisx <alexej.moiseenko@gmail.com>
2019-02-27 22:09:01 +0200

    Implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
+	if (currency < 0) {
+		return {};
+	} else if (currency > 10000) {
+		return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+	} else {
+		const example = {'H': 50, 'Q': 25, 'D': 10, 'N': 5, 'P': 1};
+		let result = {};
+		for (let item in example){
+			if (currency >= example[item]){
+				let res = currency % example[item];
+				result[item] = (currency - res) / example[item];
+				currency = res; 
+			}
+		}
+		return result;
+	}
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
 }
